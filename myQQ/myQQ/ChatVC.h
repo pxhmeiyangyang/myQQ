6143489873b1b6165a630740e67725e3231d90d6
pxh <peixinhua@unisound.com>
2016-05-23 20:29:13 +0800

    tableView实现

--- myQQ/myQQ/ChatVC.h
+++ myQQ/myQQ/ChatVC.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ChatVC.h
 //  myQQ
 //
 //  Created by pxh on 16/5/23.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ChatVC : UIViewController
 
 @end
-
---
+++ myQQ/myQQ/LinkManVC.m
@@ -0,0 +1,70 @@
+//
+//  LinkManVC.m
+//  myQQ
+//
+//  Created by pxh on 16/5/23.
+//  Copyright © 2016年 裴新华. All rights reserved.
+//
+
+#import "LinkManVC.h"
+
+@interface LinkManVC ()<UITableViewDelegate,UITableViewDataSource>
+
+//页面标示图
+@property(nonatomic,strong)UITableView* tableview;
+
+
+@end
+
+@implementation LinkManVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self.view addSubview:self.tableview];
+}
+
+#pragma mark - 懒加载 初始化
+-(UITableView *)tableview{
+    if (!_tableview) {
+        _tableview            = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT) style:UITableViewStylePlain];
+        _tableview.dataSource = self;
+        _tableview.delegate   = self;
+    }
+    return _tableview;
+}
+
+#pragma mark - tableview delagete
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 44;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+#pragma mark - tableview datasourse
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+
+-(UITableViewCell* )tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld",indexPath.row];
+    return cell;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
