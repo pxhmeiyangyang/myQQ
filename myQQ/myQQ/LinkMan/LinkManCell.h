37b965a00636e4d5fd94e11334bc61d9d308377e
pxh <peixinhua@unisound.com>
2016-05-24 11:09:30 +0800

    内容提交

---
+++ myQQ/myQQ/LinkMan/LinkManCell.h
@@ -0,0 +1,13 @@
+//
+//  LinkManCell.h
+//  myQQ
+//
+//  Created by pxh on 16/5/24.
+//  Copyright © 2016年 裴新华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LinkManCell : UITableViewCell
+
+@end
---
+++ myQQ/myQQ/LinkMan/LinkManCell.m
@@ -0,0 +1,24 @@
+//
+//  LinkManCell.m
+//  myQQ
+//
+//  Created by pxh on 16/5/24.
+//  Copyright © 2016年 裴新华. All rights reserved.
+//
+
+#import "LinkManCell.h"
+
+@implementation LinkManCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
